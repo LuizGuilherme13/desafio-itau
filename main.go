f9a6cb1b8d312036b777c6cbedc42328ce9188e6
LuizGuilherme13 <luizguilherme.bs13@hotmail.com>
2025-02-23 18:31:25 -0300

    feat: endpoint POST /transacao

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
+type Transaction struct {
+	Value    float64   `json:"valor"`
+	DateTime time.Time `json:"dataHora"`
+}
+
+func newTransaction(w http.ResponseWriter, r *http.Request) {
+	t := Transaction{}
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if t == (Transaction{}) {
+		http.Error(w, "", http.StatusUnprocessableEntity)
+		return
+	}
+
+	if t.DateTime.After(time.Now()) {
+		http.Error(w, "", http.StatusUnprocessableEntity)
+		return
+	}
+
+	if t.Value < 0 {
+		http.Error(w, "", http.StatusUnprocessableEntity)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
-	})
+	http.HandleFunc("/transacao", newTransaction)
 
 	log.Println("Server running on port:", 8080)
 
